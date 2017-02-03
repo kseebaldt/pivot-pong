3edc5e01feddea8c8e7f308d0f74743b96b162e3
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-03 12:19:30 -0800

    Deploy from travis?
    [#138879293]
    
    Signed-off-by: Noah Denton <ndenton@pivotal.io>

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+./cf-bin/cf login -a https://api.run.pivotal.io -u $CF_USERNAME -p $CF_PASSWORD -o slackersoft -s seattle-pong
+
+./cf-bin/cf rename seattle-pong seattle-pong-old
+
+./cf-bin/cf push seattle-pong
+
+./cf-bin/cf delete seattle-pong-old
