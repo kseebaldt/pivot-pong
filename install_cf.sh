114010d71b70d3b9eec690d2d19a8727e34618bd
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-07 10:41:14 -0800

    try installing cf in install instead of before
    - also use /tmp for download

--- install_cf.sh
+++ install_cf.sh
@@ -5,7 +5,7 @@ set -euv -o pipefail
 mkdir -p cf-bin
 cd cf-bin
 
-wget --progress=dot --show-progress --output-document=cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
+wget --progress=dot --show-progress --output-document=/tmp/cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
 
 echo `ls`
-tar -xzf cf-cli.tgz
+tar -xzf /tmp/cf-cli.tgz
