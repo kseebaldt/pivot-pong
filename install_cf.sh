b484bc9309c4af562e2cbb9c26105028bf00bc9f
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-07 10:25:35 -0800

    try to print things

--- install_cf.sh
+++ install_cf.sh
@@ -4,8 +4,8 @@ set -eu -o pipefail
 
 mkdir -p cf-bin
 
-wget --output-document=cf-bin/cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
+wget --progress=dot --show-progress --output-document=cf-bin/cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
 
 cd cf-bin
-ls
+echo `ls`
 tar -xzf cf-cli.tgz
