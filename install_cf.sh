de2f6cb915816ba5737407f80e1e351fbb8f74dc
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-07 10:36:23 -0800

    be in the dir when downloading

--- install_cf.sh
+++ install_cf.sh
@@ -3,9 +3,9 @@
 set -euv -o pipefail
 
 mkdir -p cf-bin
-
-wget --progress=dot --show-progress --output-document=cf-bin/cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
-
 cd cf-bin
+
+wget --progress=dot --show-progress --output-document=cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
+
 echo `ls`
 tar -xzf cf-cli.tgz
