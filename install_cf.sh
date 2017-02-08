dbed78fab80f35b74dba49720ae049405d09fcb5
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-08 11:21:22 -0800

    no background wget

--- install_cf.sh
+++ install_cf.sh
@@ -5,7 +5,7 @@ set -euv -o pipefail
 mkdir -p cf-bin
 cd cf-bin
 
-wget -O cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
+wget --no-background -O cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
 
 ls
 tar -xzf cf-cli.tgz
