1ed79e8ff4a95f5278f4bbe97baf1d38044b1b9e
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-08 14:44:37 -0800

    Only cleanup if we attempted a deploy

--- cleanup_cf.sh
+++ cleanup_cf.sh
@@ -4,6 +4,11 @@ set -eu -o pipefail
 
 ./cf-bin/cf login -a https://api.run.pivotal.io -u $CF_USERNAME -p $CF_PASSWORD -o slackersoft -s seattle-pong
 
-./cf-bin/cf delete -f seattle-pong
+./cf-bin/cf apps | grep seattle-pong-old
+if [ $? = 0 ]
+then
+    echo 'cleaning up deployment space'
+    ./cf-bin/cf delete -f seattle-pong
 
-./cf-bin/cf rename seattle-pong-old seattle-pong
+    ./cf-bin/cf rename seattle-pong-old seattle-pong
+fi
