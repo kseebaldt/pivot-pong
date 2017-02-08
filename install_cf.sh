517c78e104f9ac41b02c79167e969f511989e4bc
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-08 13:59:11 -0800

    let's try this

--- install_cf.sh
+++ install_cf.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -f cf-bin/cf ]
+then
+        echo 'already have cf'
+        exit 0
+fi
+
 set -euv -o pipefail
 
 mkdir -p cf-bin
@@ -7,5 +13,6 @@ cd cf-bin
 
 wget --no-background -O cf-cli.tgz https://cli.run.pivotal.io/stable?release=linux64-binary&source=github
 
-ls
+sleep 30
 tar -xzf cf-cli.tgz
+rm cf-cli.tgz
